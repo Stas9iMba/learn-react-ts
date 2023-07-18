5438dd32037ce1f45cc78d7409e5ad8b0160aa5a
Stas <46508483+Stas9iMba@users.noreply.github.com>
2023-07-18 20:51:10 +0700

    added TodoItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import TodoItem from "./components/TodoItem";
+
 function App() {
-
-
   return (
     <>
-
+      <TodoItem id="1" title="Learn Typescript" completed={false} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
